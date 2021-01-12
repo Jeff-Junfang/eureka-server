a617e7ced12fc91b533c78857f182f5330b68a84
wangjunf <wangjunf@tcl.com>
2021-01-12 22:48:13 +0800

    add test

--- src/test/java/com/eric/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/eric/eurekaserver/EurekaServerApplicationTests.java
@@ -3,10 +3,8 @@ package com.eric.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EurekaServerApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
